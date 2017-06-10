d1ea7118056144bd1d7e9a2c0cf38d6b3d7c7997
Everythingmaybe <55mixxx55@gmail.com>
2017-06-10 17:35:56 +0600

    FavoriteOmsk V2.0 add touch slide and click slide

--- FavoriteOmsk V.2.0 SASS/js/common.js
+++ FavoriteOmsk V.2.0 SASS/js/common.js
@@ -1,40 +1,40 @@
 var slider = {
-	picleft: new Image(),			//Картинки для предварительной загрузки
+	picleft: new Image(),			//Для предварительной загрузки
 	picright: new Image(),			//рядом стоящих картинок
 	len: placesarray.length,
 	bgimg: document.querySelector('.wrapper'),				//Объект у которого меняем фон
 	frame: Math.floor(Math.random() * placesarray.length),	//Рандом
 	set: function(image) { // установка нужного фона 
-		this.bgimg.style.backgroundImage = "url(images/img" + image + ".jpg)";
+		slider.bgimg.style.backgroundImage = "url(images/img" + image + ".jpg)";
 		if (image == 0) {
-			 this.picleft.src = "images/img" + (this.len-1) + ".jpg"
-			 this.picright.src = "images/img" + (image+1) + ".jpg";
+			 slider.picleft.src = "images/img" + (slider.len-1) + ".jpg"
+			 slider.picright.src = "images/img" + (image+1) + ".jpg";
 			}
-			else if (image == this.len-1 ) {
-				this.picright.src = "images/img" + (0) + ".jpg";
-				this.picleft.src = "images/img" + (image-1) + ".jpg";
+			else if (image == slider.len-1 ) {
+				slider.picright.src = "images/img" + (0) + ".jpg";
+				slider.picleft.src = "images/img" + (image-1) + ".jpg";
 			}
 			else {
-				this.picleft.src = "images/img" + (image-1) + ".jpg";
-				this.picright.src = "images/img" + (image+1) + ".jpg";
+				slider.picleft.src = "images/img" + (image-1) + ".jpg";
+				slider.picright.src = "images/img" + (image+1) + ".jpg";
 			}
-		this.addInfoPlace(image);
+		slider.addInfoPlace(image);
 	},
 	
 	init: function() {
-		this.set(this.frame);
+		slider.set(slider.frame);
 	},
 
 	left: function() {
-		this.frame--;
-		if(this.frame < 0) this.frame = --this.len;
-		this.set(this.frame);
+		slider.frame--;
+		if(slider.frame < 0) slider.frame = --slider.len;
+		slider.set(slider.frame);
 	},
 
 	right: function() {
-		this.frame++;
-		if(this.frame > this.len - 1) this.frame = 0;
-		this.set(this.frame);
+		slider.frame++;
+		if(slider.frame > slider.len - 1) slider.frame = 0;
+		slider.set(slider.frame);
 	},
 
 	addInfoPlace: function(a) {
@@ -73,10 +73,57 @@ var slider = {
 
 };
 
-window.onload = function() { // запуск слайдера после загрузки документа
+window.onload = function() { 
 	var
-	containerNameplace = document.querySelector('.point-name');
-	slider.init();
-	containerNameplace.onclick = slider.openInfoPlace;
+	containerNameplace = document.querySelector('.point-name'),
+	rightSlide = document.querySelector('.right'),
+	leftSlide = document.querySelector('.left');
+	
+	slider.init();							// запуск слайдера после загрузки документа
+	
+	/*Переключание слайда по клику*/
+	rightSlide.addEventListener("click", slider.right); 				
+	leftSlide.addEventListener("click", slider.left);
+	containerNameplace.addEventListener("click", slider.openInfoPlace);
+
+	/*Переключение слайда по клавишам*/
+	addEventListener("keydown", function(event) {
+    if (event.keyCode == 32 || event.keyCode == 13) {slider.openInfoPlace()};
+    if (event.keyCode == 39) {slider.right()};
+    if (event.keyCode == 37) {slider.left()};
+	})
+	
+	/*Переключение слайда для сенсорных экранов*/
+	var initialPoint,
+	finalPoint,
+	infoSection = document.querySelector('.info'),
+	mainSection = document.querySelector('.main-section');
+	mainSection.addEventListener('touchstart', function(event) {
+		event.preventDefault();
+		event.stopPropagation();
+		initialPoint=event.changedTouches[0];
+	}, false);
+	mainSection.addEventListener('touchend', function(event) {
+		event.preventDefault();
+		event.stopPropagation();
+		finalPoint=event.changedTouches[0];
+		var xAbs = Math.abs(initialPoint.pageX - finalPoint.pageX);
+		var yAbs = Math.abs(initialPoint.pageY - finalPoint.pageY);
+		if (xAbs > 20 || yAbs > 20) {
+			if (xAbs > yAbs) {
+				if (finalPoint.pageX < initialPoint.pageX){
+					/*СВАЙП ВЛЕВО*/
+					slider.right();
+				}
+				else{
+					/*СВАЙП ВПРАВО*/
+					slider.left();
+				}
+			}
+		} else {
+			infoSection.addEventListener('touchstart',
+					slider.openInfoPlace(), false);
+		}
+	}, false);
 }
 

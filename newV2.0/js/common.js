eb34d58d0993a02277af1bbe3fcb62d51ea4e12d
Everythingmaybe <55mixxx55@gmail.com>
2017-06-20 17:26:13 +0600

    add site newV2.0

---
+++ newV2.0/js/common.js
@@ -0,0 +1,10 @@
+$(function() {
+	var $container = $(".masonry-container");
+	$container.imagesLoaded(function () {
+		$container.masonry({
+			columnWidth: ".item",
+			itemSelector: ".item",
+			horizontalOrder: true,
+		});
+	});
+});

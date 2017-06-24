1b4d3a15721c4b743605dd778ac26b574518065f
Everythingmaybe <55mixxx55@gmail.com>
2017-06-24 21:35:51 +0600

    semi-final

--- newV2.0/js/common.js
+++ newV2.0/js/common.js
@@ -3,6 +3,69 @@ $(function() {
 	$('#da-slider2').cslider();
 	$('#da-slider3').cslider();
 
+	/*Smooth scrolls link*/
+	$("body").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+        top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1000);
+    });
+
+    /* Fixed menu */
+	$(window).scroll(function(){
+		if($(this).scrollTop()>120){
+			$('#nav').addClass('fixed');
+			$('.hidd').addClass('visible');
+		}
+		else if ($(this).scrollTop()<100){
+			$('#nav').removeClass('fixed');
+			$('.hidd').removeClass('visible');
+		}
+	});
+
+	/* Mobile Menu */
+
+	$( ".cross" ).hide();
+	$( ".menu-mobile" ).hide();
+
+	$( ".hamburger" ).click(function() {
+		$( ".menu-mobile" ).slideToggle( "slow", function() {
+			$( ".hamburger" ).hide();
+			$( ".cross" ).show();
+		});
+	});
+
+	$( ".cross" ).click(function() {
+		$( ".menu-mobile" ).slideToggle( "slow", function() {
+			$( ".cross" ).hide();
+			$( ".hamburger" ).show();
+		});
+	});
+
+	/*Radial Indicator */
+	radialIndicator.defaults.radius = 62;
+	radialIndicator.defaults.barColor = "#19BD9A";
+	radialIndicator.defaults.fontColor = "#81868e";
+	radialIndicator.defaults.barBgColor = "#81868e";
+	radialIndicator.defaults.fontSize = 24;
+	radialIndicator.defaults.fontWeight = "normal";
+	radialIndicator.defaults.percentage = true;
+
+	$('#indicatorBrand').radialIndicator();
+	$('#indicatorDesign').radialIndicator();
+	$('#indicatorUI').radialIndicator();
+
+	var
+	radialBrand = $('#indicatorBrand').data('radialIndicator'),
+	radialDesign = $('#indicatorDesign').data('radialIndicator'),
+	radialUI = $('#indicatorUI').data('radialIndicator');
+
+	radialBrand.value(80);
+	radialDesign.value(75);
+	radialUI.value(60);
+
+
+	/*Grid gallery*/
 	var $container = $(".masonry-container");
 	$container.imagesLoaded(function () {
 		$container.masonry({
@@ -11,5 +74,18 @@ $(function() {
 			horizontalOrder: true,
 			//gutter: 10,
 		});
+	});
+
+	/* Price-block hover */
+	$('#second-price').hover(function() {
+		$('.info-second').css("z-index","2");
+	}, function() {
+		$('.info-second').css("z-index","0");
+	});
+
+	/* Map open*/
+	$(".map").click(function() {
+		$(".map").fadeOut('400', function() {	
+		});;
 	});
 });

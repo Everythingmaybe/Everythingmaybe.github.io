205dccaaf2537c62b7cf1a1b4118716823baf775
Everythingmaybe <55mixxx55@gmail.com>
2017-10-08 17:43:44 +0600

    Add new project.

---
+++ Estimate/app/js/common.js
@@ -0,0 +1,32 @@
+$(function() {
+    const mySiema = new Siema({
+        selector: '.siema',
+        selectorWrapper: '.wrapper-btn',
+        duration: 200,
+        easing: 'ease-out',
+        perPage: 1,
+        startIndex: 0,
+        draggable: true,
+        threshold: 20,
+        loop: true,
+        onInit: () => {},
+        onChange: () => {},
+	});
+
+    // Add a function that generates pagination to prototype
+    Siema.prototype.addPagination = function() {
+    	const wrapperBtn = document.createElement('div');
+    		wrapperBtn.classList.add('wrapper-btn');
+    		this.selector.appendChild(wrapperBtn);
+        for (let i = 0; i < this.innerElements.length; i++) {
+            const btn = document.createElement('button');
+            btn.textContent = i;
+            btn.addEventListener('click', () => this.goTo(i));
+            document.querySelector('.wrapper-btn').appendChild(btn);
+        }
+    }
+
+// Trigger pagination creator
+    mySiema.addPagination();
+});
+
